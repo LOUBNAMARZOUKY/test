bb95b02e11c0b32b8c606d53492a8e49d9bf3fff
LOUBNAMARZOUKY <loubnamarzouky0@gmail.com>
2018-12-18 15:17:21 +0100

    first commit

---
+++ src/main/java/emi/ac/ma/App.java
@@ -0,0 +1,14 @@
+package emi.ac.ma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!! " );
+        System.out.println( "Hello World! " );
+    }
+}
